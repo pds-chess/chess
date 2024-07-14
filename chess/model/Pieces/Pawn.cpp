21f94ed3e55713431cd8fc20314babf4e65045ea
DRB <davibahia_eletrica@hotmail.com>
2024-07-14 00:01:54 -0300

    Update Pawn.cpp

--- chess/model/Pieces/Pawn.cpp
+++ chess/model/Pieces/Pawn.cpp
@@ -7,15 +7,15 @@ class Pawn: Piece{
         Pawn(Coordinates initial_coords, Color color) : moved_(false), Piece::Piece(initial_coords, color, *getBoard()){
         };
         bool validateMove(Coordinates final_coordinates) const {
-            int incremento = 0;
+            int multiplier = 0;
             if(Piece::getColor())
-                 int incremento = -1;
-            else int incremento = 1;
-            int DeltaRow = (final_coordinates.getRow() - Piece::getCoords().getRow())*incremento;
-            int DeltaCol = (final_coordinates.getCol() - Piece::getCoords().getCol())*incremento;
+                 int multiplier = -1;
+            else int multiplier = 1;
+            int DeltaRow = (final_coordinates.getRow() - Piece::getCoords().getRow())*multiplier;
+            int DeltaCol = (final_coordinates.getCol() - Piece::getCoords().getCol())*multiplier;
             if (DeltaRow >0 && DeltaRow <= 2  && (DeltaCol==0)) {
                 for (int i=2; i<=3 && (i<3 || !moved_); i++)
-                    if (getBoard()->getPiece(Coordinates (Piece::getCoords().getRow()+i*incremento, Piece::getCoords().getCol()))!=nullptr)
+                    if (getBoard()->getPiece(Coordinates (Piece::getCoords().getRow()+i*multiplier, Piece::getCoords().getCol()))!=nullptr)
                         return false;
             }
             else
@@ -23,5 +23,7 @@ class Pawn: Piece{
                 return true;
             return false;
         };
-        void en_passant();
+        void en_passant() {
+            
+        };
 };
