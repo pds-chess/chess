12dae90bc7f2a1c5cdd1cc0938df81dce8e3647d
DRB <davibahia_eletrica@hotmail.com>
2024-07-20 17:28:55 -0300

    Update Bishop.cpp

--- chess/model/Pieces/Bishop.cpp
+++ chess/model/Pieces/Bishop.cpp
@@ -13,18 +13,22 @@
         int RowP =getCoords().getRow(), ColP = getCoords().getCol();
         if (RowF+ColF==Secundary_Diagonal || RowF-ColF==Primary_Diagonal) {
             int multiplier_i=0, multiplier_j=0;
-            if(RowP>RowF && ColP>ColF)
+            if(RowP>RowF && ColP>ColF) {
                 multiplier_i=-1;
                 multiplier_j=-1;
-            if(RowP<RowF && ColP<ColF)
+            }
+            if(RowP<RowF && ColP<ColF) {
                 multiplier_i=1;
                 multiplier_j=1;
-            if(RowP>RowF && ColP<ColF)
+            }
+            if(RowP>RowF && ColP<ColF) {
                 multiplier_i=-1;
                 multiplier_j=1;
-            if(RowP<RowF && ColP>ColF)
+            }
+            if(RowP<RowF && ColP>ColF) {
                 multiplier_i=1;
                 multiplier_j=-1;
+            }
             for (int i=2, j=2;i<=8 && j<=8 && i!=RowF+multiplier_i && j!=ColF+multiplier_j; i++, j++) {
                 Piece* PieceF= getBoard()->getPiece(Coordinates (RowP+i*multiplier_i,ColP+j*multiplier_j));
                 if (PieceF->getColor()!=getColor() && PieceF!=nullptr && PieceF->getCoords().getCol()==ColF && PieceF->getCoords().getRow()==RowF)
