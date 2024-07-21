532a26840e64b98228cc38d5b34f7b7f0e76b928
DRB <davibahia_eletrica@hotmail.com>
2024-07-21 10:34:29 -0300

    Update Bishop.cpp

--- chess/model/Pieces/Bishop.cpp
+++ chess/model/Pieces/Bishop.cpp
@@ -29,7 +29,7 @@
                 multiplier_i=1;
                 multiplier_j=-1;
             }
-            for (int i=2, j=2;i<=8 && j<=8 && i!=RowF+multiplier_i && j!=ColF+multiplier_j; i++, j++) {
+            for (int i=2, j=2;i<=8 && j<=8; i++, j++) {
                 Piece* PieceF= getBoard()->getPiece(Coordinates (RowP+i*multiplier_i,ColP+j*multiplier_j));
                 if (PieceF->getColor()!=getColor() && PieceF!=nullptr && PieceF->getCoords().getCol()==ColF && PieceF->getCoords().getRow()==RowF)
                     return true;
