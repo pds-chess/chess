eaea78558741e76d9e6e13f31da58dc614fa8eb5
Breno-dt <brenodteixeira@gmail.com>
2024-07-28 10:23:57 -0300

    validateEnPassant

--- chess/model/Pieces/Pawn.cpp
+++ chess/model/Pieces/Pawn.cpp
@@ -6,12 +6,19 @@ Pawn::Pawn(Coordinates initial_coords, Color color, Board *board)
     moved_ = false;
 }
 
+void Pawn::movePiece(Coordinates final_coordinates){
+    Piece::movePiece(final_coordinates);
+    if(moved_ == true)
+        movedTwice_ = true;
+    moved_ = true;
+}
+
 bool Pawn::validateMove(Coordinates final_coordinates) const{
     int multiplier = 0;
-    if (getColor()==1)
-        int multiplier = -1;
+    if (getColor()==White)
+        multiplier = -1;
     else
-        int multiplier = 1;
+        multiplier = 1;
     int DeltaRow = (final_coordinates.getRow() - getCoords().getRow()) * multiplier;
     int DeltaCol = (final_coordinates.getCol() - getCoords().getCol()) * multiplier;
     if (DeltaRow > 0 && DeltaRow <= 2 && (DeltaCol == 0)){
@@ -24,8 +31,32 @@ bool Pawn::validateMove(Coordinates final_coordinates) const{
     return false;
 }
 
-void Pawn::en_passant() {
+bool Pawn::validateEnPassant(Coordinates final_coordinates){
+    int mult = 0;
+    int finalRow = 0;
+    if (getColor()==White){
+        mult = -1;
+        finalRow = 2;
+    }else{
+        mult = 1;
+        finalRow = 5;
+    }
+    int DeltaRow = (final_coordinates.getRow() - getCoords().getRow()) * mult;
+    int DeltaCol = (final_coordinates.getCol() - getCoords().getCol()) * mult;
+    if (final_coordinates.getRow() == finalRow && getBoard()->getPiece(final_coordinates) == nullptr && DeltaRow == 1 && DeltaCol == 1)
+    {
+        Piece* target = getBoard()->getPiece(Coordinates(final_coordinates.getRow()-mult, final_coordinates.getCol()));
+        if (dynamic_cast<Pawn*>(target) != NULL && dynamic_cast<Pawn*>(target)->movedTwice_ == false)
+            return true;
+        else
+            return false;
+    }else
+        return false;
+    
+}
 
+void Pawn::enPassant(Coordinates final_coordinates) {
+    
 }
 
 char Pawn::getPieceChar() const{
--- include/Pawn.hpp
+++ include/Pawn.hpp
@@ -5,9 +5,12 @@
 class Pawn: public Piece{
     private:
         bool moved_;
+        bool movedTwice_;
     public:
         Pawn(Coordinates initial_coords, Color color, Board* board);
         virtual bool validateMove(Coordinates final_coordinates) const;
-        void en_passant();
+        void movePiece(Coordinates final_coordinates);
+        bool validateEnPassant(Coordinates final_coordinates);
+        void enPassant(Coordinates final_coordinates);
         virtual char getPieceChar() const;
 };
