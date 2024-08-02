483e1610c11e5cca483c682fc8a3cb77e3ce181b
lebraga1 <lebragacool@gmail.com>
2024-08-02 17:24:03 -0300

    Update temp test

--- test/testezinho.cpp
+++ test/testezinho.cpp
@@ -1,8 +1,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-#include "/home/brenodt/PDS2/chess/include/King.hpp"
-#include "/home/brenodt/PDS2/chess/include/Match.hpp"
+#include "King.hpp"
+#include "Board.hpp"
 
 //Comando para compilar:
 //g++ include/Color.hpp include/GameState.hpp chess/model/util/Coordinates.cpp include/Move.hpp include/PieceType.hpp chess/model/Pieces/Piece.cpp chess/model/Pieces/PieceFactory.cpp chess/model/Player.cpp chess/model/Pieces/King.cpp chess/model/Pieces/Queen.cpp chess/model/Pieces/Knight.cpp chess/model/Pieces/Pawn.cpp chess/model/Pieces/Bishop.cpp chess/model/Pieces/Rook.cpp chess/model/Board.cpp chess/model/History.cpp chess/model/Match.cpp test/testezinho.cpp -I include -o test/testezinho
