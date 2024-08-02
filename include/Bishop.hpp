169e07d69f683a7d8dca006ac7c223e18d634ee9
Breno-dt <brenodteixeira@gmail.com>
2024-08-02 09:50:08 -0300

    compile command update

--- include/Bishop.hpp
+++ include/Bishop.hpp
@@ -6,7 +6,6 @@ class Bishop: public Piece{
     public:
         Bishop(const Coordinates& initial_coords, Color color, const Board& board);
         bool validateMove(const Coordinates& final_coordinates) const override;
-        char getPieceChar() const override;
         PieceType getType() const override;
     protected:
         char getPieceChar() const override;
