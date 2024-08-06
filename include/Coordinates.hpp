11a5b1090c2335b80e8b5059bffcce6ca8adceb3
Breno-dt <brenodteixeira@gmail.com>
2024-08-06 17:36:45 -0300

    documentation

--- include/Coordinates.hpp
+++ include/Coordinates.hpp
@@ -8,10 +8,33 @@ private:
     int row_;
     int column_;
 public:
-    Coordinates();
-    Coordinates(int row, int column);
-    int getRow() const;
-    int getCol() const;
-    std::string toString();
-    void fromString(const std::string& str);
+/**
+   * @brief Construct a Coordinate.
+   *
+   * @param row represents the row in a matrix(7,7) 
+   * @param column represents the column in a matrix(7,7)
+   */
+        Coordinates(int row, int column);
+    /**
+   * @brief Construct an empty Coordinate.
+   */
+        Coordinates();
+    /**
+   * @brief Return the row of a Coordinate.
+   */
+        int getRow() const;
+    /**
+   * @brief Return the column of a Coordinate.
+   */
+        int getCol() const;
+    /**
+   * @brief Transform a Coordinate into string.
+   */
+        std::string toString();
+    /**
+   * @brief Transform a string into Coordinate, if it's possible.
+   *
+   * @param str string that will be transformed.
+   */
+        void fromString(const std::string& str);
 };
--- include/History.hpp
+++ include/History.hpp
@@ -8,12 +8,29 @@
 #include <list>
 
 class History{
-    private:
-        const std::string path_to_file_;
-    
-    public:
+private:
+    const std::string path_to_file_;
+public:
+/**
+   * @brief Construct match history, with every move done until the end.
+   *
+   * @param path represents where that history will be saved. 
+   */
         History(std::string path);
+/**
+   * @brief Save the match identifier and all the moves done.
+   *
+   * @param moves a list with all the moves that has occured in the match.
+   */
         void saveMatch(std::list<Move> moves);
+/**
+   * @brief return all the moves of a especified match.
+   *
+   * @param game_id the identifier of a match.
+   */
         std::list<Move> getMoves(int game_id) const;
+/**
+   * @brief return all the games played.
+   */
         std::list<int> getGames() const;
 };
